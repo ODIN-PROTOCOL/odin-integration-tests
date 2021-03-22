3193443457a7e9ed57c219c96d8fa6ceb2ff53dd
StepanTita <stepan.tytarenko@nure.ua>
2021-03-22 15:57:45 +0200

    update oracle params tests

--- scripts/info.js
+++ scripts/info.js
@@ -36,16 +36,18 @@ async function main() {
 
     // Example queries
     //const balances = await client.bank.balances(myAddress);
+    console.log('Validators: ', await client.staking.validators());
     console.log('Distribution parameters: ', await client.distribution.parameters());
-    console.log('Gov proposals: ', await client.gov.proposals());
+    console.log('Gov proposals: ', JSON.stringify(await client.gov.proposals(), null, 2));
     console.log('Mint inflation: ', await client.mint.inflation());
     console.log('Slashing signingInfos: ', await client.slashing.signingInfos());
     console.log('Staking redelegations: ', await client.staking.redelegations());
     console.log('Supply totalAll: ', await client.supply.totalAll());
-    console.log('Fee pool:', await client.distribution.communityPool())
-    console.log('Data providers pool', (await getDataProvidersPool()).data)
-    console.log('Current rate', (await getCurrentRate()).data)
-    console.log('Coinswap params', (await getParams()).data)
+    console.log('Fee pool:', await client.distribution.communityPool());
+    console.log('Data providers pool', (await getDataProvidersPool()).data);
+    console.log('Current rate', (await getCurrentRate()).data);
+    console.log('Coinswap params', (await getParams()).data);
+    console.log('Mint params: ', await client.mint.parameters());
 }
 
 main()
