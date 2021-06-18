a032ac9d66c132aeacc12946b32b00a9a53f09b0
stepan <stepan.tytarenko@nure.ua>
2021-06-18 13:30:50 +0300

    add blocks and tx queries

---
+++ src/scripts/blocks.js
@@ -0,0 +1,22 @@
+const {
+    Tendermint34Client
+} = require("@cosmjs/tendermint-rpc");
+
+const config = require('../../config.json');
+
+function err(reason) {
+    console.log(reason);
+}
+
+async function main() {
+    const client = await Tendermint34Client.connect(config.rpc);
+
+    // TODO: get rewards (and commission) from here
+    console.log('Block results by height:', await client.blockResults(10).catch(err));
+
+    console.log('Block by height:', await  client.block(10).catch(err));
+
+    console.log('Blockchain:', await  client.blockchain(10, 12).catch(err));
+}
+
+main()
--- src/scripts/queryTx.js
+++ src/scripts/queryTx.js
@@ -1,6 +1,6 @@
-const {
-    StargateClient
-} = require("@cosmjs/stargate");
+const {Tx} = require("@cosmjs/stargate/build/codec/cosmos/tx/v1beta1/tx");
+
+const {fromHex} = require("@cosmjs/encoding")
 
 const {
     Tendermint34Client
@@ -13,10 +13,20 @@ function err(reason) {
 }
 
 async function main() {
-    const client = new StargateClient(await Tendermint34Client.connect(config.rpc));
+    const client = await Tendermint34Client.connect(config.rpc);
     const txHash = "4FBA932E551DBD71B7CF29FAF058A7EE3B16628E9FF373411D40AC653A5CCCDB";
     // Tx
-    console.log('Tx by hash:', await client.getTx(txHash).catch(err));
+    // console.log('Tx by hash:', await client.tx({hash: fromHex(txHash)}).catch(err));
+
+    const txs = await client.txSearch({query: `tx.height = 10`}).catch(err);
+    console.log('Tx search:', txs);
+
+    console.log('Tx raw:', txs.txs[0]);
+
+    const decodedTx = Tx.decode(txs.txs[0].tx);
+    console.log('Tx parsed:', decodedTx);
+
+    console.log('Messages:', decodedTx.body.messages);
 }
 
 main()
