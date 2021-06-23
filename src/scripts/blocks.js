552f80444091d03d22515beeef92c4189c701e87
stepan <stepan.tytarenko@nure.ua>
2021-06-23 11:35:22 +0300

    add hash decoding to blocks

--- src/scripts/blocks.js
+++ src/scripts/blocks.js
@@ -3,6 +3,7 @@ const {
 } = require("@cosmjs/tendermint-rpc");
 
 const config = require('../../config.json');
+const {toHex} = require("@cosmjs/encoding");
 
 function err(reason) {
     console.log(reason);
@@ -14,9 +15,12 @@ async function main() {
     // TODO: get rewards (and commission) from here
     console.log('Block results by height:', await client.blockResults(10).catch(err));
 
-    console.log('Block by height:', await  client.block(10).catch(err));
+    console.log('Block by height:', await client.block(10).catch(err));
 
-    console.log('Blockchain:', await  client.blockchain(10, 12).catch(err));
+    console.log('Blockchain:', await client.blockchain(10, 12).catch(err));
+
+    let block = await client.block(10).catch(err);
+    console.log(toHex(block.blockId.hash).toUpperCase());
 }
 
 main()
