4063efc731c0f20e40c6c038fbe3a841ac70d970
slandymani <slandyman9@gmail.com>
2022-08-31 16:58:31 +0300

    changes to create proposal tx

--- src/scripts/createProposal.js
+++ src/scripts/createProposal.js
@@ -19,13 +19,18 @@ async function main() {
         content: {
             typeUrl: "/cosmos.params.v1beta1.ParameterChangeProposal",
             value: ParameterChangeProposal.encode({
-                title: "Update mint air",
-                description: "my awesome proposal",
+                title: "Mint 17.5m Geos for cosmos ecosystem Airdrop",
+                description: "### Summary \nWe are requesting to mint 17500000 GEO to be minted on the ODIN chain for the cosmos Ecosystem airdrop.\n\n### Details\n50% of the total GEO supply will be minted on the ODIN chain in the next few months.\nGEO, the first ODT token, will be distributed among the following:\n\n1. ODIN Stakers &  liquidity providers ($ODIN/Osmos)\n2. Atom Stakers\n3. Osmosis Stakers\n\nMinimum amount & snapshot dates to be announced.\n\nThank you!",
                 changes: [
                     {
                         subspace: "mint",
-                        key: "MintAir",
-                        value: "true"
+                        key: "MaxAllowedMintVolume",
+                        value: '[{"denom":"mGeo", "amount":"17500000000000"}, {"denom":"unit_denom", "amount":"amount"}]'
+                    },
+                    {
+                        subspace: "mint",
+                        key: "AllowedMintDenoms",
+                        value: '[{"token_unit_denom":"mGeo", "token_denom":"geo"},{"token_unit_denom":"unit_denom", "token_denom":"denom"}]'
                     }
                 ]
             }).finish()
